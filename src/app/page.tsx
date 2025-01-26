ded81768b739d8716acbff598922c04ad3ba6fff
Ambreen Sheikh <ambreensheikh900@gmail.com>
2025-01-26 21:30:28 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import FeaturedProducts from '@/components/featuredProducts'
+import React from 'react'
+
+
+const fetchData = () => {
+  return (
+    <div>
+<FeaturedProducts/>
+    </div>
+  )
+}
+
+export default fetchData
